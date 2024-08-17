9a0919363fcbc30693e2654e160529bdc8a94f09
CayCall <131669034+CayCall@users.noreply.github.com>
2024-08-17 18:04:12 +0200

    added extra creativeMovement

--- Team_12_MinecraftCreative/Assets/Scripts/player/playerMovement.cs
+++ Team_12_MinecraftCreative/Assets/Scripts/player/playerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class playerMovement : MonoBehaviour
 {
@@ -9,9 +10,11 @@ public class playerMovement : MonoBehaviour
         public float walkSpeed = 6f;
         public float runSpeed = 12f;
         public float JumpPower = 7f;
+        public float creativeSpeed = 10f;
         public float gravity = 10f;
 
         [SerializeField] private bool isWalking = false;
+        public bool isCreativeMode = false;
 
         private Vector3 initialPosition;
         private Vector3 velocity = Vector3.zero; // For SmoothDamp
@@ -20,7 +23,7 @@ public class playerMovement : MonoBehaviour
     #region MouseAndCameraVariables
         [Header("Mouse look & Camera")]
         public float lookSpeed = 2f;
-        public float lookXLimit = 45f;
+        public float lookYLimit = 45f;
         public Camera playerCamera;
         public float amplitude = 0.1f;  // Height of bobbing effect
         public float frequency = 1.0f;  // Speed of bobbing effect
@@ -56,65 +59,99 @@ public class playerMovement : MonoBehaviour
     {
         HandleMovement();
         HandleRotation();
+        HandleCreativeMovement();
     }
 
     private void HandleMovement()
     {
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-
-        // Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-        isWalking = curSpeedX != 0 || curSpeedY != 0;
-
-        // Jumping
-        if (Input.GetButton("Jump") && canMove && _characterController.isGrounded)
+        if (!isCreativeMode)
         {
-            moveDirection.y = JumpPower;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            // Shift to run
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            isWalking = curSpeedX != 0 || curSpeedY != 0;
+
+            // Jumping
+            if (Input.GetButton("Jump") && canMove && _characterController.isGrounded)
+            {
+                moveDirection.y = JumpPower;
+            }
+            else
+            {
+                moveDirection.y = movementDirectionY;
+            }
+
+            if (!_characterController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            _characterController.Move(moveDirection * Time.deltaTime);
+                ..08/.
+            Vector3 targetPosition = initialPosition;
+            if (isWalking)
+            {
+                targetPosition = new Vector3(
+                    initialPosition.x,
+                    initialPosition.y + Mathf.Sin(Time.time * frequency) * amplitude,
+                    initialPosition.z
+                );
+            }
+
+            playerCamera.transform.localPosition = Vector3.SmoothDamp(
+                playerCamera.transform.localPosition,
+                targetPosition,
+                ref velocity,
+                smoothTime
+            );
+        }
+    }
+
+    private void HandleCreativeMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ToggleCreativeMode();
+        }
+
+        if (isCreativeMode)
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            movement = transform.TransformDirection(movement) * creativeSpeed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                movement.y += creativeSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                movement.y -= creativeSpeed * Time.deltaTime;
+            }
+
+            _characterController.Move(movement);
+        }
+    }
+
+    private void ToggleCreativeMode()
+    {
+        isCreativeMode = !isCreativeMode;
+
+        if (isCreativeMode)
+        {
+            gravity = 0;
         }
         else
         {
-            moveDirection.y = movementDirectionY;
+            gravity = 100f; 
         }
-
-        if (!_characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-            
-        }
-        
-
-        _characterController.Move(moveDirection * Time.deltaTime);
-
-
-        // Smoothly interpolate between current and target positions
-        Vector3 targetPosition = initialPosition;
-        if (isWalking)
-        {
-            targetPosition = new Vector3(
-                initialPosition.x,
-                initialPosition.y + Mathf.Sin(Time.time * frequency) * amplitude,
-                initialPosition.z
-            );
-        }
-
-        playerCamera.transform.localPosition = Vector3.SmoothDamp(
-            playerCamera.transform.localPosition,
-            targetPosition,
-            ref velocity,
-            smoothTime
-        );
-    }
-
-    // Method to set whether the player is walking or not
-    public void SetWalking(bool walking)
-    {
-        isWalking = walking;
     }
 
     private void HandleRotation()
@@ -122,7 +159,7 @@ public class playerMovement : MonoBehaviour
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            rotationX = Mathf.Clamp(rotationX, -lookYLimit, lookYLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
